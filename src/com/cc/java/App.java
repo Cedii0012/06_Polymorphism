ac644213fb9fe21bfdd63d952e5d11df1761f627
Peter <schmidt.multimedia@gmail.com>
2021-08-31 09:34:55 +0200

    update / Subklassen / Overriding

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -1,8 +1,20 @@
 package com.cc.java;
 public class App {
     public static void main(String[] args) {
-      // to do
-    
+      
+    HoneyBee worker = new Worker();
+    HoneyBee queen = new Queen();
+    HoneyBee drone = new Drone();
+
+    ausgabe(worker.doYourJob());
+    ausgabe(queen.doYourJob());
+    ausgabe(drone.doYourJob());
+
+    ausgabe("------------------");
+
+    ausgabe(worker.fly());
+    ausgabe(queen.fly());
+    ausgabe(drone.fly());
 
     }
 
